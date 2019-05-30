3640089585facd21af6be0d3a05097243ef30793
Marcus Eduardo <marcusedu7@gmail.com>
2019-05-30 15:10:28 -0300

    transactions bugfix

--- functions/index.js
+++ functions/index.js
@@ -1,160 +1,132 @@
-/* eslint-disable promise/always-return */
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
 var firestore = admin.firestore();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
 exports.updateAddReactions = functions.firestore.document('reacts/{reactId}')
     .onCreate((snap, context) => {
         let react = snap.data();
-        console.log(react);
-        firestore.runTransaction(trans => {
+        firestore.runTransaction(async (trans) => {
             let userRef = firestore.collection('users').doc(react.user_id);
-            trans.get(userRef).then((userSnap) => {
-                let newReactCount = (userSnap.data().num_reactions || 0) + 1;
-                trans.set(userRef, { 'num_reactions': newReactCount });
-            }).catch((cat) => { });
-        }).catch((cat) => { });
+            let userSnap = await trans.get(userRef);
+            let newCount = (userSnap.data().num_reactions || 0) + 1;
+            return trans.update(userRef, { 'num_reactions': newCount });
+        });
         if (react.publication_id) {
-            firestore.runTransaction((trans) => {
+            firestore.runTransaction(async (trans) => {
                 let userRef = firestore.collection('publications').doc(react.publication_id);
-                trans.get(userRef).then((userSnap) => {
-                    let newReactCount = (userSnap.data().num_reacts || 0) + 1;
-                    trans.set(userRef, { 'num_reacts': newReactCount });
-                }).catch((cat) => { });
+                let userSnap = await trans.get(userRef);
+                let newCount = (userSnap.data().num_reacts || 0) + 1;
+                return trans.update(userRef, { 'num_reacts': newCount });
             });
         }
         if (react.reply_id) {
-            firestore.runTransaction((trans) => {
+            firestore.runTransaction(async (trans) => {
                 let userRef = firestore.collection('replies').doc(react.reply_id);
-                trans.get(userRef).then((userSnap) => {
-                    let newReactCount = (userSnap.data().num_reacts || 0) + 1;
-                    trans.set(userRef, { 'num_reacts': newReactCount });
-                }).catch((cat) => { });
-            }).catch((cat) => { });
+                const userSnap = await trans.get(userRef);
+                let newCount = (userSnap.data().num_reacts || 0) + 1;
+                return trans.update(userRef, { 'num_reacts': newCount });
+            });
         }
     });
 exports.updateRemoveReactions = functions.firestore.document('reacts/{reactId}')
     .onDelete((snap, context) => {
         let react = snap.data();
-        console.log(react);
-        firestore.runTransaction((trans) => {
+        firestore.runTransaction(async (trans) => {
             let userRef = firestore.collection('users').doc(react.user_id);
-            trans.get(userRef).then((userSnap) => {
-                let newReactCount = (userSnap.data().num_reactions || 0) - 1;
-                trans.set(userRef, { 'num_reactions': newReactCount });
-            }).catch((cat) => { });
-        }).catch((cat) => { });
+            let userSnap = await trans.get(userRef);
+            let newCount = (userSnap.data().num_reactions || 0) - 1;
+            return trans.update(userRef, { 'num_reactions': newCount });
+        });
         if (react.publication_id) {
-            firestore.runTransaction((trans) => {
+            firestore.runTransaction(async (trans) => {
                 let userRef = firestore.collection('publications').doc(react.publication_id);
-                trans.get(userRef).then((userSnap) => {
-                    let newReactCount = (userSnap.data().num_reacts || 0) - 1;
-                    trans.set(userRef, { 'num_reacts': newReactCount });
-                }).catch((cat) => { });
-            }).catch((cat) => { });
+                let userSnap = await trans.get(userRef);
+                let newCount = (userSnap.data().num_reacts || 0) - 1;
+                return trans.update(userRef, { 'num_reacts': newCount });
+            });
         }
         if (react.reply_id) {
-            firestore.runTransaction((trans) => {
+            firestore.runTransaction(async (trans) => {
                 let userRef = firestore.collection('replies').doc(react.reply_id);
-                trans.get(userRef).then((userSnap) => {
-                    let newReactCount = (userSnap.data().num_reacts || 0) - 1;
-                    trans.set(userRef, { 'num_reacts': newReactCount });
-                }).catch((cat) => { });
-            }).catch((cat) => { });
+                const userSnap = await trans.get(userRef);
+                let newCount = (userSnap.data().num_reacts || 0) - 1;
+                return trans.update(userRef, { 'num_reacts': newCount });
+            });
         }
     });
 
 exports.updateAddPubs = functions.firestore.document('publications/{publicationId}')
     .onCreate((snap, context) => {
         let pub = snap.data();
-        console.log(pub);
-        firestore.runTransaction((trans) => {
+        firestore.runTransaction(async (trans) => {
             let userRef = firestore.collection('users').doc(pub.user_id);
-            trans.get(userRef).then((userSnap) => {
-                let newCount = (userSnap.data().num_publications || 0) + 1;
-                trans.set(userRef, { 'num_publications': newCount });
-            }).catch((cat) => { });
-        }).catch((cat) => { });
-        if (pub.publication_id) {
-            firestore.runTransaction((trans) => {
-                let userRef = firestore.collection('publications').doc(pub.publication_id);
-                trans.get(userRef).then((userSnap) => {
-                    let newReactCount = (userSnap.data().num_reacts || 0) - 1;
-                    trans.set(userRef, { 'num_replies': newReactCount });
-                }).catch((cat) => { });
-            }).catch((cat) => { });
-        }
-        if (pub.reply_id) {
-            firestore.runTransaction((trans) => {
-                let userRef = firestore.collection('replies').doc(pub.reply_id);
-                trans.get(userRef).then((userSnap) => {
-                    let newReactCount = (userSnap.data().num_reacts || 0) - 1;
-                    trans.set(userRef, { 'num_replies': newReactCount });
-                }).catch((cat) => { });
-            }).catch((cat) => { });
-        }
+            const userSnap = await trans.get(userRef);
+            let newCount = (userSnap.data().num_publications || 0) + 1;
+            return trans.update(userRef, { 'num_publications': newCount });
+        });
     });
 exports.updateRemovePubs = functions.firestore.document('publications/{publicationId}')
     .onDelete((snap, context) => {
         let pub = snap.data();
-        console.log(pub);
-        firestore.runTransaction((trans) => {
+        firestore.runTransaction(async (trans) => {
             let userRef = firestore.collection('users').doc(pub.user_id);
-            trans.get(userRef).then((userSnap) => {
-                let newCount = (userSnap.data().num_publications || 0) - 1;
-                trans.set(userRef, { 'num_publications': newCount });
-            }).catch((cat) => { });
-        }).catch((cat) => { });
-        if (pub.publication_id) {
-            firestore.runTransaction((trans) => {
-                let userRef = firestore.collection('publications').doc(pub.publication_id);
-                trans.get(userRef).then((userSnap) => {
-                    let newReactCount = (userSnap.data().num_reacts || 0) - 1;
-                    trans.set(userRef, { 'num_replies': newReactCount });
-                }).catch((cat) => { });
-            }).catch((cat) => { });
-        }
-        if (pub.reply_id) {
-            firestore.runTransaction((trans) => {
-                let userRef = firestore.collection('replies').doc(pub.reply_id);
-                trans.get(userRef).then((userSnap) => {
-                    let newReactCount = (userSnap.data().num_reacts || 0) - 1;
-                    trans.set(userRef, { 'num_replies': newReactCount });
-                }).catch((cat) => { });
-            }).catch((cat) => { });
-        }
+            const userSnap = await trans.get(userRef);
+            let newCount = (userSnap.data().num_publications || 1) - 1;
+            return trans.update(userRef, { 'num_publications': newCount });
+        });
     });
 
 exports.updateAddReplies = functions.firestore.document('replies/{replyId}')
     .onCreate((snap, context) => {
         let reply = snap.data();
-        console.log(reply);
-        firestore.runTransaction((trans) => {
+        firestore.runTransaction(async (trans) => {
             let userRef = firestore.collection('users').doc(reply.user_id);
-            trans.get(userRef).then((userSnap) => {
-                let newCount = (userSnap.data().num_publications || 0) + 1;
-                trans.set(userRef, { 'num_replies': newCount });
-            }).catch((cat) => { });
-        }).catch((cat) => { });
+            let userSnap = await trans.get(userRef);
+            let newCount = (userSnap.data().num_replies || 0) + 1;
+            return trans.update(userRef, { 'num_replies': newCount });
+        });
+        if (reply.publication_id) {
+            firestore.runTransaction(async (trans) => {
+                let pubRef = firestore.collection('publications').doc(reply.publication_id);
+                let pubSnap = await trans.get(pubRef);
+                let newCount = (pubSnap.data().num_replies || 0) + 1;
+                return trans.update(pubRef, { 'num_replies': newCount });
+            });
+        }
+        if (reply.reply_id) {
+            firestore.runTransaction(async (trans) => {
+                let replyRef = firestore.collection('replies').doc(reply.reply_id);
+                const replySnap = await trans.get(replyRef);
+                let newCount = (replySnap.data().num_replies || 0) + 1;
+                return trans.update(replyRef, { 'num_replies': newCount });
+            });
+        }
     });
 exports.updateRemoveReplies = functions.firestore.document('replies/{replyId}')
     .onDelete((snap, context) => {
         let reply = snap.data();
-        console.log(reply);
-        firestore.runTransaction((trans) => {
+        firestore.runTransaction(async (trans) => {
             let userRef = firestore.collection('users').doc(reply.user_id);
-            trans.get(userRef).then((userSnap) => {
-                let newCount = (userSnap.data().num_publications || 0) - 1;
-                trans.set(userRef, { 'num_publications': newCount });
-            }).catch((cat) => { });
-        }).catch((cat) => { });
+            let userSnap = await trans.get(userRef);
+            let newCount = (userSnap.data().num_replies || 1) - 1;
+            return trans.update(userRef, { 'num_replies': newCount });
+        });
+        if (reply.publication_id) {
+            firestore.runTransaction(async (trans) => {
+                let pubRef = firestore.collection('publications').doc(reply.publication_id);
+                let pubSnap = await trans.get(pubRef);
+                let newCount = (pubSnap.data().num_replies || 1) - 1;
+                return trans.update(pubRef, { 'num_replies': newCount });
+            });
+        }
+        if (reply.reply_id) {
+            firestore.runTransaction(async (trans) => {
+                let replyRef = firestore.collection('replies').doc(reply.reply_id);
+                const replySnap = await trans.get(replyRef);
+                let newCount = (replySnap.data().num_replies || 1) - 1;
+                return trans.update(replyRef, { 'num_replies': newCount });
+            });
+        }
     });
